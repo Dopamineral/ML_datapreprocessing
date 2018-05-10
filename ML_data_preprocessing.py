58f6e86268f07a58d7ee50bdecad588b41dbed5e
Dopamineral <35575269+Dopamineral@users.noreply.github.com>
2018-05-10 18:36:28 +0200

    Add files via upload

---
+++ ML_data_preprocessing.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing dataset
+dataset = pd.read_csv("Data.csv")
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,3]
+
+#missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = "NaN", strategy = "mean",axis = 0)
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+#categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+#splitting into training and testing data sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2)
+
+#Scaling variables! (normalization, standardization)
+from sklearn.preprocessing import StandardScaler
+scaler_X = StandardScaler()
+X_train = scaler_X.fit_transform(X_train)
+X_test = scaler_X.transform(X_test) 
+# don't have to fit the scaler to X_test because it has already been fitted
+# to X_train, so it will have the same effect on X_test
+
+
+
